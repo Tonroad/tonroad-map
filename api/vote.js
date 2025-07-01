2e99e26ab6a700e2ae5d2055a25f38bb1911e277
Sergey Mut <Sergey.Mut@HLSoftware.de>
2025-07-01 14:11:37 +0200

    fix file extension

---
+++ api/vote.js
@@ -0,0 +1,20 @@
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const { city, userId } = req.body;
+
+      console.log(`Голос от ${userId} за ${city}`);
+
+      res.status(200).json({
+        success: true,
+        voted: city,
+        winner: city
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ success: false });
+    }
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
